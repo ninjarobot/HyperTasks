2712dacb297e975ec3d9a13601aca411ff6a60ab
Dave Curylo <curylod@asme.org>
2019-08-28 22:14:06 -0400

    Initial Commit

---
+++ HyperTasks/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HyperTasks
+//
+//  Created by David Curylo on 8/28/19.
+//  Copyright © 2019 Dave Curylo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HyperTasksTests/HyperTasksTests.swift
@@ -0,0 +1,34 @@
+//
+//  HyperTasksTests.swift
+//  HyperTasksTests
+//
+//  Created by David Curylo on 8/28/19.
+//  Copyright © 2019 Dave Curylo. All rights reserved.
+//
+
+import XCTest
+@testable import HyperTasks
+
+class HyperTasksTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
